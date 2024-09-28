ad78132ed1bfe6d9ee5904030ecec9c44807b83b
Antoine Bertin <antoine@bertin.io>
2024-09-28 22:20:30 +0200

    feat: add achordion

--- qmk/users/diaoul/diaoul.c
+++ qmk/users/diaoul/diaoul.c
@@ -10,3 +10,44 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
             return QUICK_TAP_TERM;
     }
 }
+
+// Process record
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef ACHORDION_ENABLE
+    if (!process_achordion(keycode, record)) {
+        return false;
+    }
+#endif
+
+    return true;
+}
+
+// Matrix scan
+void matrix_scan_user(void) {
+#ifdef ACHORDION_ENABLE
+    achordion_task();
+#endif
+}
+
+// Achordion
+#ifdef ACHORDION_ENABLE
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+    return 800;
+}
+bool achordion_eager_mod(uint8_t mod) {
+    // Make all mods eager for more responsive mouse and OLED display
+    switch (mod) {
+        case MOD_LGUI:
+        case MOD_LALT:
+        case MOD_LSFT:
+        case MOD_LCTL:
+        case MOD_RCTL:
+        case MOD_RSFT:
+        case MOD_RGUI:
+        case MOD_RALT:
+            return true;
+        default:
+            return false;
+    }
+}
+#endif
--- qmk/users/diaoul/diaoul.h
+++ qmk/users/diaoul/diaoul.h
@@ -7,3 +7,10 @@
 #ifdef OLED_ENABLE
 #    include "features/oled.h"
 #endif
+#ifdef ACHORDION_ENABLE
+#    include "features/getreuer/achordion.h"
+#endif
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record);
+bool     process_record_user(uint16_t keycode, keyrecord_t *record);
+void     matrix_scan_user(void);

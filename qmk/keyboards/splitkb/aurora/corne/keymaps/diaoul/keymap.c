1dc7adffdd56e61639400dbb2ef49395d69094b3
Antoine Bertin <antoine@bertin.io>
2025-07-12 15:39:36 +0200

    fix: rework symbol layer

--- qmk/keyboards/splitkb/aurora/corne/keymaps/diaoul/keymap.c
+++ qmk/keyboards/splitkb/aurora/corne/keymaps/diaoul/keymap.c
@@ -3,24 +3,24 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_split_3x6_3(
-  KC_GRV  , KC_Q , KC_W , KC_F , KC_P   , KC_B    ,                          KC_J    , KC_L   , KC_U    , KC_Y   , KC_QUOT , KC_MINS,
-  KC_ESC  , HR_A , HR_R , HR_S , HR_T   , KC_G    ,                          KC_M    , HR_N   , HR_E    , HR_I   , HR_O    , KC_QUES,
-  KC_LPRN , KC_Z , KC_X , KC_C , KC_D   , KC_V    ,                          KC_K    , KC_H   , KC_COMM , KC_DOT , KC_SLSH , KC_RPRN,
-                                 KC_ESC , NUM_TAB , NAV_SPC ,     SYM_BSPC , SYM_ENT , QK_REP
+  KC_GRV  , KC_Q , KC_W , KC_F , KC_P   , KC_B    ,                          KC_J    , KC_L    , KC_U    , KC_Y   , KC_QUOT , KC_MINS,
+  KC_ESC  , HR_A , HR_R , HR_S , HR_T   , KC_G    ,                          KC_M    , HR_N    , HR_E    , HR_I   , HR_O    , KC_QUES,
+  OS_LSFT , KC_Z , KC_X , KC_C , KC_D   , KC_V    ,                          KC_K    , KC_H    , KC_COMM , KC_DOT , KC_SLSH , KC_UNDS,
+                                 QK_REP , NUM_TAB , NAV_SPC ,     SYM_BSPC , SYM_ENT , KC_COLN
 ),
 
 [SYM] = LAYOUT_split_3x6_3(
-  KC_EXLM , KC_AMPR , KC_COLN , KC_PLUS , KC_PERC , KC_TILD ,                        _______ , _______ , _______ , _______ , _______ , _______,
-  KC_HASH , KC_CIRC , KC_DLR  , KC_UNDS , KC_EQL  , KC_ASTR ,                        _______ , KC_RCTL , KC_RSFT , KC_RGUI , KC_RALT , _______,
-  KC_AT   , KC_LT   , KC_PIPE , KC_MINS , KC_GT   , KC_SLSH ,                        _______ , _______ , _______ , _______ , _______ , _______,
-                                          KC_BSLS , KC_TAB  , KC_SPC ,     _______ , _______ , _______
+  KC_GRV  , KC_AT   , KC_LT   , KC_GT   , KC_MINS , KC_PIPE ,                        KC_CIRC , KC_LBRC , KC_RBRC , KC_DLR  , KC_QUOT , _______,
+  _______ , KC_EXLM , KC_ASTR , KC_UNDS , KC_EQL  , KC_AMPR ,                        KC_HASH , KC_LPRN , KC_RPRN , KC_SCLN , KC_DQUO , _______,
+  _______ , KC_TILD , KC_PLUS , KC_LCBR , KC_RCBR , KC_PERC ,                        _______ , KC_COLN , KC_COMM , KC_DOT  , KC_SLSH , _______,
+                                          _______ , KC_BSLS , KC_SPC ,     _______ , _______ , _______
 ),
 
 [NUM] = LAYOUT_split_3x6_3(
-  _______ , _______ , _______ , _______ , _______ , _______ ,                         KC_PERC , KC_7   , KC_8 , KC_9 , KC_COLN  , KC_COMM,
-  _______ , KC_LALT , KC_LGUI , KC_LSFT , KC_LCTL , _______ ,                         KC_PLUS , KC_1   , KC_2 , KC_3 , KC_MINUS , KC_DOT ,
-  _______ , _______ , _______ , _______ , _______ , _______ ,                         KC_ASTR , KC_4   , KC_5 , KC_6 , KC_SLSH  , _______,
-                                          _______ , _______ , _______ ,     KC_BSPC , KC_0    , KC_DOT
+  _______ , _______ , _______ , _______ , _______ , _______ ,                         KC_PERC , KC_7    , KC_8 , KC_9 , KC_COLN  , KC_COMM,
+  _______ , KC_LALT , KC_LGUI , KC_LSFT , KC_LCTL , _______ ,                         KC_PLUS , KC_1    , KC_2 , KC_3 , KC_MINUS , KC_DOT ,
+  _______ , _______ , _______ , _______ , _______ , _______ ,                         KC_ASTR , KC_4    , KC_5 , KC_6 , KC_SLSH  , _______,
+                                          _______ , _______ , _______ ,     KC_BSPC , KC_0    , _______
 ),
 
 [NAV] = LAYOUT_split_3x6_3(

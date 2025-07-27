c23e5f4ef48566efc485cf53389e408470c17a60
Antoine Bertin <antoine@bertin.io>
2025-07-27 23:45:36 +0200

    feat: firmware optimization and compilation warnings

--- qmk/users/diaoul/rules.mk
+++ qmk/users/diaoul/rules.mk
@@ -1,7 +1,15 @@
 # Firmware size optimizations (see https://docs.qmk.fm/squeezing_avr)
+LTO_ENABLE = yes
+# CONSOLE_ENABLE = no
+# COMMAND_ENABLE = no
+# MOUSEKEY_ENABLE = no
+# EXTRAKEY_ENABLE = no
 MAGIC_ENABLE = no
 GRAVE_ESC_ENABLE = no
 SPACE_CADET_ENABLE = no
+
+# Fix compilation warning for Aurora Corne
+EXTRAFLAGS += -flto=auto
 
 # Extra features
 REPEAT_KEY_ENABLE ?= yes
